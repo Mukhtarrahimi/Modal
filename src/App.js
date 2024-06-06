0f4187a0fbc8ba48779e5e49a3fd111d6574260f
Mukhtarrahimi <mukhtarrahimi110@gmail.com>
2024-06-07 00:39:27 +0430

    modal code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import './App.css';
+import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
+import Modla from "./component/Modla";
 
 function App() {
+  let [open, setOpen] = useState(false);
   return (
     <div className="App">
-    <button className='btn btn-danger'></button>
+      <button className="btn btn-danger" onClick={() => setOpen(true)}>
+        <FaTrashAlt /> Delete
+      </button>
+      <Modla open={open} onClose={() => setOpen(false)}>
+        <div className="text-center w-56">
+          <FaTrashAlt className="text-4xl text-red-600 block mx-auto" />
+          <div className="mx-auto my-4 w-48">
+            <h3 className="text-lg font-bold text-gray-800">Confirm Delete</h3>
+            <p className="text-sm text-gray-500">
+              Are you sure you want to delete this item?
+            </p>
+          </div>
+          <div className="flex gap-4">
+            <button
+              className="btn btn-light"
+              onClick={() => setOpen(false)}
+            >
+              Cancel
+            </button>
+            <button
+              className="btn btn-danger"
+              onClick={() => setOpen(false)}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </Modla>
     </div>
   );
 }
--- src/component/Modla.js
+++ src/component/Modla.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { IoMdClose } from "react-icons/io";
 
-function Modla({open, onclose, children}) {
+function Modla({ open, onClose, children }) {
   return (
-    
-  )
+    <div
+      onClick={onClose}
+      className={`flex items-center justify-center inset-0 fixed transition-colors ${
+        open ? "visible bg-black/20" : "invisible"
+      }`}
+    >
+      <div onClick={(e) => e.stopPropagation()} className={`bg-white rounded-lg p-6 transition-all ${open? 'scale-100 opacity-100' : 'scale-125 opacity-0'}`}>
+        <button onClick={onClose} className="absolute right-2 top-2 bg-gray-100 text-gray-500 hover:bg-gray-300 duration-500 rounded p-1"><IoMdClose/></button>
+      {children}
+      </div>
+    </div>
+  );
 }
 
-export default Modla
+export default Modla;
